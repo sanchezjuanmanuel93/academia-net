6aa281d6d1ef777576325210033ad91d4d4e2462
fede.93c@gmail.com <fede.93c@gmail.com@03eebda7-322c-b966-8bc7-0810c309a789>
2014-10-27 23:54:45 +0000

    archivo faltante

---
+++ trunk/ academia-net --username fede.93c@gmail.com/Presentacion/frmAltaInscripcion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Entidades;
+using Negocio;
+
+namespace Presentacion
+{
+    public partial class frmAltaInscripcion : Form
+    {
+        public frmAltaInscripcion(Persona p)
+        {
+            InitializeComponent();
+            persona = p;
+        }
+
+        Persona persona;
+        ControladorInscripciones controladorInscripciones = new ControladorInscripciones();
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (controladorInscripciones.agregarInscripcion(persona.Legajo, ((Materias)cmbMateria.SelectedItem).nroMateria.ToString(), DateTime.Now))
+            {
+                MessageBox.Show("OK");
+                this.Close();
+            }
+            else
+                MessageBox.Show("Error");
+        }
+
+        private void frmAltaInscripcion_Load(object sender, EventArgs e)
+
+        {
+            
+            cmbMateria.DataSource = controladorInscripciones.getMateriasSinInscripcion(persona.Legajo);
+            cmbMateria.DisplayMember = "Nombre";
+        }
+
+
+    }
+}
