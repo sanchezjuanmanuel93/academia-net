87ae39586a80e6503c8b7ef328e4e5cef94be04d
sanchez.juanmy <sanchez.juanmy@gmail.com>
2014-10-29 19:01:56 +0000

    Se agrega el formulario de personas y el alta persona. FALTA el edit y que se recargue la grilla cuando se hace una baja.

---  academia-net --username fede.93c@gmail.com/PresentacionWeb/frmMaterias.aspx
+++  academia-net --username fede.93c@gmail.com/PresentacionWeb/frmMaterias.aspx
@@ -15,6 +15,16 @@
                         <asp:BoundField DataField="Nombre" HeaderText="Nombre" 
                             SortExpression="Nombre" />
                     </Columns>
+                    <EditRowStyle BackColor="#2461BF" />
+                    <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                    <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                    <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                    <RowStyle BackColor="#EFF3FB" />
+                    <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                    <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                    <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                    <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                    <SortedDescendingHeaderStyle BackColor="#4870BE" />
                 </asp:GridView>
                 <asp:ObjectDataSource ID="odsMaterias" runat="server" 
                     SelectMethod="getMaterias" TypeName="Negocio.ControladorMaterias">
---  academia-net --username fede.93c@gmail.com/PresentacionWeb/frmPersonas.aspx.cs
+++  academia-net --username fede.93c@gmail.com/PresentacionWeb/frmPersonas.aspx.cs
@@ -4,11 +4,59 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Entidades;
+using Negocio;
 
 public partial class frmPersonas : System.Web.UI.Page
 {
+    Persona persona;
+    ControladorPersonas controladorPersonas = new ControladorPersonas();
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        persona = (Persona)Session["persona"];
+    }
+    
+    
+    protected void btnAlta_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/frmAltaPersona.aspx");
+    }
+    
+    
+    protected void btnBaja_Click(object sender, EventArgs e)
+    {
+        if (dgvPersonas.SelectedIndex != -1)
+        {
+            controladorPersonas.eliminarPersona(dgvPersonas.SelectedRow.Cells[1].Text);
+            lblMensaje.Text = "Se dió de baja correctamente " + dgvPersonas.SelectedRow.Cells[3].Text + " " + dgvPersonas.SelectedRow.Cells[2].Text;
+            //llenarGrilla();
+        }
+        else
+        {
+            lblMensaje.Text = "Se debe seleccionar alguna Persona";
+        }
+        
+    
+    }
+    protected void btnCancelar_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/frmPrincipal.aspx");
+    }
 
+    void llenarGrilla()
+    {
+
+        try
+        {
+            if (persona != null)
+            {
+                dgvPersonas.DataSource = controladorPersonas.getPersonas();
+                dgvPersonas.DataBind();
+            }
+        }
+        catch (Exception)
+        {
+        }
     }
 }
